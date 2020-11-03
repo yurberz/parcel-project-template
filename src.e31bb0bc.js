2a36ef194074d90b9bec801095a5407abe6929be
Yurii Berezynets <yuriiberezynets@gmail.com>
2020-11-03 09:27:26 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62264" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64361" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
