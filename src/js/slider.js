2602fc618a2475fb08096928d7d93ffd9e3c4501
IrinaMai <irinam3340@gmail.com>
2020-11-03 17:16:30 +0300

    slider for 3 fotos done review commited

---
+++ src/js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        speed: 250,
+        easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+        // inffinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
+        draggable: true,
+        swipe: true,
+        touchTreshhold: 3,
+        touchMove: false,
+
+
+    });
+});
